feeb436b3234eaa5c988859562090490a7c2bb09
Jonathan Capps <cappsy@gmail.com>
2024-11-06 01:36:04 +0000

    replaced all placeholder distro logos. lots of bad ones, but better than the fallback

--- tzyfetch.sh
+++ tzyfetch.sh
@@ -24,89 +24,89 @@ lightcyan='[1;96m'
 # Distro config
 declare -A alldistros
 
-alldistros[almalinux]="$red ?_?"
+alldistros[almalinux]="$blue {X}"
 alldistros[alpine]="$blue <^>"
-alldistros[altlinux]="$yellow r,l"
+alldistros[altlinux]="$yellow r,l" #?
 alldistros[amzn]="$yellow |Y|"
 alldistros[anarchy]="$blue /-\\"
 alldistros[arch]="$blue /.\\"
 alldistros[arch32]="$blue /.\\"
 alldistros[archcraft]="$blue /.\\"
 alldistros[arcolinux]="$blue /,\\"
-alldistros[arkane]="$red ?_?"
+alldistros[arkane]="$red ^,^"
 alldistros[artix]="$blue />\\"
-alldistros[bazzite]="$purple Γ+)"
+alldistros[bazzite]="$purple Γ+)" #?
 alldistros[blackarch]="$lightblue /|\\"
-alldistros[blendos]="$red ?_?"
-alldistros[bodhi]="$red ?_?"
-alldistros[cachyos]="$green C:\`"
-alldistros[centos]="$red ?_?"
-alldistros[centos_stream]="$red ?_?"
-alldistros[chimera]="$red {*}"
-alldistros[clear-linux-os]="$red ?_?"
+alldistros[blendos]="$green (B)"
+alldistros[bodhi]="$green '\'" #?
+alldistros[cachyos]="$green C:\`" #?
+alldistros[centos]="$blue <+>"
+alldistros[centos_stream]="$blue <+>"
+alldistros[chimera]="$red °w°"
+alldistros[clear-linux-os]="$lightblue |\Γ" #?
 alldistros[debian]="$red (@)"
-alldistros[Deepin]="$red ?_?"
-alldistros[devuan]="$red ?_?"
-alldistros[dragonfly]="$red ?_?"
-alldistros[elementary]="$red ?_?"
-alldistros[endeavouros]="$red ?_?"
-alldistros[endless]="$red ?_?"
-alldistros[eurolinux]="$red ?_?"
-alldistros[exherbo]="$red ?_?"
+alldistros[Deepin]="$lightblue (%)" #?
+alldistros[devuan]="$red >cv" #?
+alldistros[dragonfly]="$red =I="
+alldistros[elementary]="$lightblue (e)"
+alldistros[endeavouros]="$purple |D)"
+alldistros[endless]="$yellow d\p"
+alldistros[eurolinux]="$blue (-)" #?
+alldistros[exherbo]="$white °o°"
 alldistros[fedora]="$blue (f)"
 alldistros[fedoraremixforwsl]="$blue (f)"
-alldistros[freebsd]="$red ?_?"
-alldistros[funtoo]="$red ?_?"
-alldistros[garuda]="$red ?_?"
-alldistros[gentoo]="$gray )°)"
-alldistros[ghostbsd]="$red ?_?"
-alldistros[gnoppix]="$red ?_?"
-alldistros[hyperbola]="$red ?_?"
-alldistros[kali]="$red ?_?"
-alldistros[kaos]="$red ?_?"
+alldistros[freebsd]="$red ^O^"
+alldistros[funtoo]="$purple f°°"
+alldistros[garuda]="$blue o\`>"
+alldistros[gentoo]="$gray >°>"
+alldistros[ghostbsd]="$blue (G)" #?
+alldistros[gnoppix]="$blue (G)" #?
+alldistros[hyperbola]="$white /H/"
+alldistros[kali]="$lightblue 3c\`" #?
+alldistros[kaos]="$blue l<.?"
 alldistros[linuxmint]="$green lm)"
-alldistros[mageia]="$red ?_?"
-alldistros[manjaro]="$red ?_?"
-alldistros[manjaro-arm]="$red ?_?"
-alldistros[miraclelinux]="$red ?_?"
-alldistros[neon]="$red ?_?"
-alldistros[nilrt]="$red ?_?"
-alldistros[nixos]="$red ?_?"
-alldistros[nobara]="$red ?_?"
-alldistros[ol]="$red ?_?"
-alldistros[omnios]="$red ?_?"
-alldistros[openmandriva]="$red ?_?"
-alldistros[opensuse]="$green @~>"
-alldistros[suse]="$green @~>"
+alldistros[mageia]="$blue (°)" #?
+alldistros[manjaro]="$green (M)" #?
+alldistros[manjaro-arm]="$green (M)" #?
+alldistros[miraclelinux]="$green l|l"
+alldistros[neon]="$lightblue (•)"
+alldistros[nilrt]="$green [n]"
+alldistros[nixos]="$blue <=>" #?
+alldistros[nobara]="$white n•>"
+alldistros[ol]="$red (_)" #?
+alldistros[omnios]="$white \\\`>" #?
+alldistros[openmandriva]="$blue ((o"
+alldistros[opensuse]="$green @n>"
+alldistros[suse]="$green @n>"
 alldistros[opensuse-leap]="$green \\^/"
 alldistros[opensuse-tumbleweed]="$lightblue o/o"
-alldistros[openwrt]="$red ?_?"
-alldistros[parrot]="$red ?_?"
-alldistros[pclinuxos]="$red ?_?"
-alldistros[pengwin]="$red ?_?"
-alldistros[photon]="$red ?_?"
-alldistros[pisilinux]="$red ?_?"
+alldistros[openwrt]="$lightblue (V)" #?
+alldistros[parrot]="$green <\\^"
+alldistros[pclinuxos]="$blue (v)" #?
+alldistros[pengwin]="$purple (p)" #?
+alldistros[photon]="$gray (:)"
+alldistros[pisilinux]="$purple ^v^"
 alldistros[pop]="$lightblue P!_"
-alldistros[pureos]="$red ?_?"
-alldistros[rebornos]="$red ?_?"
-alldistros[redox-os]="$red ?_?"
-alldistros[rhel]="$red ?_?"
-alldistros[rocky]="$red ?_?"
-alldistros[slackware]="$red ?_?"
-alldistros[sled]="$red ?_?"
-alldistros[sles]="$red ?_?"
+alldistros[pureos]="$blue POS" #?
+alldistros[rebornos]="$lightblue <X>"
+alldistros[redox-os]="$lightblue (R)"
+alldistros[rhel]="$red _n_"
+alldistros[rocky]="$green (/,)"
+alldistros[slackware]="$lightblue (S)"
+alldistros[sled]="$green @n>"
+alldistros[sles]="$green @n>"
 alldistros[solaris]="$red \\|/"
-alldistros[solus]="$red ?_?"
+alldistros[solus]="$blue |\\)"
 alldistros[steamos]="$blue •))"
-alldistros[tails]="$purple :D="
-alldistros[tinycore]="$red ?_?"
-alldistros[trisquel]="$red ?_?"
+alldistros[tails]="$purple :Dc"
+alldistros[tinycore]="$gray (/)"
+alldistros[trisquel]="$blue @Y@"
 alldistros[ubuntu]="$yellow {•}"
-alldistros[ultramarine]="$red ?_?"
-alldistros[vanillaos]="$red ?_?"
-alldistros[virtuozzo]="$red ?_?"
-alldistros[void]="$red ?_?"
-alldistros[wolfi]="$red	?_?"
+alldistros[ultramarine]="$blue (;)" #?
+alldistros[vanillaos]="$yellow >*<"
+alldistros[virtuozzo]="$red ?\\z/"
+alldistros[void]="$green (\\)"
+alldistros[wolfi]="$red	,O,"
 alldistros[zorin]="$blue <Z>"
 
 # Get host
@@ -133,21 +133,30 @@ else
     distro_kernel="${KERNEL}"
 fi
 
-# find distro(s) and send to output
+# Find distro(s) and send to output
 for key in "${!alldistros[@]}" ; do
     set -- ${alldistros[$key]}
+    # If in 'all' mode
     if [[ "$distro_id" == "all" ]] ; then
         cat <<EOF
   $1$2  ${key}${reset}
 EOF
+    # If distro has a match
     elif [[ "$distro_id" == "$key" ]] ; then
+        # If it's a match from the argument
         if [[ "$distro_name" == "" ]] ; then
             cat <<EOF
+
   $1$2  ${key}${reset}
+        
 EOF
+            exit
+        # Otherwise, print the full fetch based on the user system
         else
             cat <<EOF
-  $1$2  ${distro_name} ${bold}${gray}:: ${white}${USER}$1@${white}${HOST} ${gray}:: ${white}${distro_kernel} ${gray}:: ${white}${UPS} sec${reset}
+
+  $1$2  ${distro_name} ${bold}${gray}:: ${white}${USER}${gray}@${white}${HOST} ${gray}:: ${white}${distro_kernel} ${gray}:: ${white}${UPS}s${reset}
+
 EOF
         fi
         exit
